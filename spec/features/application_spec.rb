64e2d32a5378ca7a0667d13f82a1c92ca159478b
Sooyung Kim <kimx1279@gmail.com>
2023-12-15 09:12:34 -0700

    Add test

---
+++ spec/features/application_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe '/' do
+
+  describe 'as a visitor' do
+    describe 'when I visit /' do
+      it 'has select field for nation and search for member button' do
+        visit '/'
+
+        expect(page).to have_select('nation')
+        expect(page).to have_button('Search For Members')
+      end
+
+
+      it 'shows the number of people who live in the fire nation and detailed info for first 25 members' do
+        visit '/'
+        select('Fire Nation', from: 'nation')
+        click_button('Search For Members')
+
+        expect(current_path).to eq('/search')
+      end
+    end
+  end
+end
